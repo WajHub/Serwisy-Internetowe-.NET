b101b8ee33910aeccf9343edd32da1b143e63236
Hubert Wajda <hubert.wajda03@gmail.com>
2025-11-24 21:30:11 +0100

    exporting to csv file;

--- IoTService/src/IoTService.Api/Controllers/DriveSystemSensorController.cs
+++ IoTService/src/IoTService.Api/Controllers/DriveSystemSensorController.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using IoTService.Application.Services;
 using IoTService.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -29,6 +30,41 @@ public class DriveSystemSensorController : ControllerBase
         var data = await _driveSystemSensorService.FindAllByInstanceAsync(instance, fromDate, toDate);
         return Results.Ok(data);
     }
+    
+    [HttpGet("export/csv")]
+    public async Task<IActionResult> ExportCsv([FromQuery] string? instance, [FromQuery] DateTime? fromDate, [FromQuery] DateTime? toDate)
+    {
+        var data = instance is null
+            ? await _driveSystemSensorService.FindAllAsync(fromDate, toDate)
+            : await _driveSystemSensorService.FindAllByInstanceAsync(instance, fromDate, toDate);
+
+        var csv = GenerateCsv(data);
+
+        return File(
+            Encoding.UTF8.GetBytes(csv),
+            "text/csv",
+            "drive_system_export.csv"
+        );
+    }
+
+    private string GenerateCsv(IEnumerable<DriveSystemSensor> data)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine("SensorId,Rpm,Torque,MotorTemperature,CurrentDraw,Timestamp");
+
+        foreach (var r in data)
+        {
+            sb.AppendLine($"{r.SensorId}," +
+                          $"{r.Data.Rpm}," +
+                          $"{r.Data.Torque}," +
+                          $"{r.Data.MotorTemperature}," +
+                          $"{r.Data.CurrentDraw}," +
+                          $"{r.Timestamp:O}");
+        }
+
+        return sb.ToString();
+    }
+
 
 
 }
